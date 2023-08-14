55d966a1ccc511b70e055708dc7c85e44ed79234
AnikaBushra <anikabushra024@gmail.com>
2023-08-14 22:12:58 +0600

    blog part added

---
+++ src/components/RecentBlogs/RecentBlog.jsx
@@ -0,0 +1,28 @@
+import { AiOutlineArrowRight } from "react-icons/ai";
+
+const RecentBlog = ({ blog }) => {
+    const { image, details, author_image, author_name, date, blog_name } = blog;
+
+    return (
+        <div className="rounded-lg bg-gray-100 shadow-md shadow-slate-400">
+            <img className="h-60 w-full rounded-lg mb-6" src={image} alt="language learning" />
+            <div className="text-justify p-4">
+                <h3 className="text-2xl h-20">{blog_name}</h3>
+
+                <p>{details.slice(0, 250)} . . .</p>
+                <div className="flex items-center font-bold mb-10 hover:text-blue-400">
+                    <button>Read More</button><AiOutlineArrowRight className="mt-1 ms-2 font-bold"></AiOutlineArrowRight>
+                </div>
+            </div>
+            <div className="text-left grid grid-cols-4 w-3/5  p-4">
+                <img className="w-10 h-9 rounded-full my-auto" src={author_image} alt="" />
+                <div className="col-span-3">
+                    <p>{author_name}</p>
+                    <p>{date}</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default RecentBlog;
--- src/components/RecentBlogs/RecentBlogs.jsx
+++ src/components/RecentBlogs/RecentBlogs.jsx
@@ -1,42 +1,26 @@
-import { AiOutlineArrowRight } from "react-icons/ai";
+import { useEffect, useState } from "react";
+
+import RecentBlog from "./RecentBlog";
 
 const RecentBlogs = () => {
+    const [blogs, setBlogs] = useState([])
+    useEffect(() => {
+        fetch('http://localhost:5000/blog')
+            .then(res => res.json())
+            .then(data => setBlogs(data))
+    }, [])
     return (
-        <div>
+        <div className="bg-gray-200">
             <div className="hero-content text-center flex flex-col md:m-10">
                 <h1 className="text-3xl my-10">Recent Blogs And Articles</h1>
                 <div>
-                    <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
-                        <div className="text-left">
-                            <img
-                                className="h-1/3"
-                                src="https://img.freepik.com/free-vector/hand-drawn-english-school-illustration_23-2149491453.jpg?size=626&ext=jpg&ga=GA1.2.1941617285.1627660604&semt=ais" alt="kids playing" />
-                            <div>
-                                <h2 className="text-3xl text-bold my-4"><i>8 Most Asked Questions About The Ielts Exam</i></h2>
-                                <p>IELTS is a standardized test designed to assess the language proficiency of non-native English speakers. It measures their ability to listen, read, write, and speak in English. IELTS scores are often used for university admissions, visa applications, and employment purposes.</p>
-                                <button className="font-bold my-2 flex">Read More<AiOutlineArrowRight className="m-2"></AiOutlineArrowRight></button>
-                            </div>
-                        </div>
-                        <div className="text-left">
-                            <img
-                                className="h-1/3"
-                                src="https://img.freepik.com/free-photo/young-english-teacher-doing-her-class-online_23-2149019784.jpg?size=626&ext=jpg&ga=GA1.2.1941617285.1627660604&semt=ais" alt="kids playing" />
-                            <div>
-                                <h2 className="text-3xl text-bold my-4"><i>8 Most Asked Questions About The Ielts Exam</i></h2>
-                                <p>IELTS is a standardized test designed to assess the language proficiency of non-native English speakers. It measures their ability to listen, read, write, and speak in English. IELTS scores are often used for university admissions, visa applications, and employment purposes.</p>
-                                <button className="font-bold my-2 flex">Read More<AiOutlineArrowRight className="m-2"></AiOutlineArrowRight></button>
-                            </div>
-                        </div>
-                        <div className="text-left">
-                            <img
-                                className="h-1/3"
-                                src="https://img.freepik.com/free-vector/hand-drawn-english-school-illustration_23-2149491453.jpg?size=626&ext=jpg&ga=GA1.2.1941617285.1627660604&semt=ais" alt="kids playing" />
-                            <div>
-                                <h2 className="text-3xl text-bold my-4"><i>8 Most Asked Questions About The Ielts Exam</i></h2>
-                                <p>IELTS is a standardized test designed to assess the language proficiency of non-native English speakers. It measures their ability to listen, read, write, and speak in English. IELTS scores are often used for university admissions, visa applications, and employment purposes.</p>
-                                <button className="font-bold my-2 flex">Read More<AiOutlineArrowRight className="m-2"></AiOutlineArrowRight></button>
-                            </div>
-                        </div>
+                    <div className="grid grid-cols-1 md:grid-cols-3 gap-5 m-2 ">
+                        {
+                            blogs.map(blog => <RecentBlog
+                                key={blog._id}
+                                blog={blog}
+                            ></RecentBlog>)
+                        }
                     </div>
                 </div>
             </div>
